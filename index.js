9d29f0b4dbe8c701768c7d670ae9f4ff6bdca410
Brian <briandsalemi@gmail.com>
2018-10-09 01:40:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return("Sorry, we don't have a credit card on file for you.");
   } else { 
-    cart.slice(0, cart.length);
+    cart = [];
     return(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
   }
 }
